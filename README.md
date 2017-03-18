2d322d0053617737e554daf6068b5dd4f158ad3b
Artem Chernyaev <askanio234@gmail.com>
2017-03-18 14:29:40 +0300

    Add initial implementation of the script

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
-# Sites Monitoring Utility
+# Частотный анализ
 
-[TODO. There will be project description]
+Скрипт проверяет домены из текстового файла.
+Для того что бы пройти проверку сервер должен ответить с кодом 200 и имя должно быть проплачено на более, чем месяц вперед
 
-# Project Goals
+# Как запустить
+
+Запуск на Linux:
+Перед запуском установить зависимости:
+```#!bash
+pip install -r requirements.txt
+```
+Один обязательный параметр ```filepath``` - путь до текстового файла с доменами
+
+```#!bash
+
+$ E:\Users\projects\17_sites_monitoring>python check_sites_health.py site_list.txt
+https://www.google.com статус - Ok!
+https://www.yandex.com статус - Ok!
+https://www.tesla.com статус - Ok!
+https://learn.python.ru статус - Ok!
+https://www.youtube.com статус - Ok!
+https://www.alfabank.ru статус - Ok!
+https://www.github.com статус - Ok!
+https://www.python.org статус - Ok!
+
+```
+
+Запуск на Windows происходит аналогично.
+
+# Цели проекта
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,68 @@
-def load_urls4check(path):
-    pass
+import os
+import datetime
+import argparse
+import requests
+import whois
+
+
+ONE_MONTH = datetime.timedelta(days=30)
+
+
+def load_text_file(path):
+    if os.path.exists(path):
+        with open(path, "r") as file_handler:
+            return file_handler.readlines()
+
+
+def load_urls(file):
+    return [line.strip() for line in file]
+
 
 def is_server_respond_with_200(url):
-    pass
+    request = requests.get(url)
+    return request.status_code == 200
 
-def get_domain_expiration_date(domain_name):
-    pass
+
+def get_domain_expiration_date(url):
+    if "https://www." in url:
+        domain_name = url.replace("https://www.", "")
+    elif "http://www." in url:
+        domain_name = url.replace("http://www.", "")
+    else:
+        domain_name = url
+    request = whois.whois(domain_name)
+    if type(request.expiration_date) is list:
+        return request.expiration_date[0]
+    return request.expiration_date
+
+
+def is_domain_expiration_date_ok(exp_date, today, timedelta):
+    return (exp_date - timedelta) >= today
+
+
+def parse_criteria(expiration_status, response_status):
+    if expiration_status and response_status:
+        return "Ok!"
+    else:
+        return "NOT Ok! attention is required!"
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath", help="Путь до файла с url")
+    args = parser.parse_args()
+    file_with_urls = load_text_file(args.filepath)
+    if file_with_urls is not None:
+        today = datetime.datetime.today()
+        urls = load_urls(file_with_urls)
+        for url in urls:
+            expiration_status = is_domain_expiration_date_ok(
+                                            get_domain_expiration_date(url),
+                                            today,
+                                            ONE_MONTH)
+            response_status = is_server_respond_with_200(url)
+            print("{} статус - {}".format(url, parse_criteria(
+                                                            expiration_status,
+                                                            response_status)))
+    else:
+        print("Неккоректный путь до файла")
